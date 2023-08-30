c9c5efeec66152850350d8f1c52c1bc13754f86e
DarioCodes1 <dario.pinatar@gmail.com>
2023-08-30 11:44:51 +0100

    added boilerplate code

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("hello")
+import pygame
+from sys import exit 
+
+pygame.init()
+screen = pygame.display .set_mode((480,360))
+pygame.display.set_caption("dungeon game")
+clock = pygame.time.Clock()
+test_font = pygame.font.Font("Pixeltype.ttf", 50)
+
+text_surface = test_font.render("Dungeon", None, "white")
+#background_surface = pygame.image.load("")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    #screen.blit(background_surface,(0,0))
+    screen.blit(text_surface,(0,0))
+
+    pygame.display.update()
+    clock.tick(60)
