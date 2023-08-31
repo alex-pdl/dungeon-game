f3bcd1eca3475a8cbba02a00be21d62b28ed65a9
alex-pdl <apodlesny00@gmail.com>
2023-08-31 13:17:03 +0100

    Added code which imports player sprites

--- characters.py
+++ characters.py
@@ -3,9 +3,35 @@ import pygame
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
+
+        #importing "still" sprites
+        self.player_still_front = pygame.image.load("assets/sprites/soldier/soldier_front.png")
+        self.player_still_back = pygame.image.load("assets/sprites/soldier/soldier_back.png")
+        self.player_still_left = pygame.image.load("assets/sprites/soldier/soldier_left.png")
+        self.player_still_right = pygame.image.load("assets/sprites/soldier/soldier_right.png")
+        #importing sprite for movement animation
+        player_front_move_0 = pygame.image.load("assets/sprites/soldier/soldier_front_move_0.png")
+        player_front_move_1 = pygame.image.load("assets/sprites/soldier/soldier_front_move_1.png")
+        player_back_move_0 = pygame.image.load("assets/sprites/soldier/soldier_back_move_0.png")
+        player_back_move_1 = pygame.image.load("assets/sprites/soldier/soldier_back_move_1.png")
+        player_right_move_0 = pygame.image.load("assets/sprites/soldier/soldier_right_move_0.png")
+        player_right_move_1 = pygame.image.load("assets/sprites/soldier/soldier_right_move_1.png")
+        player_left_move_0 = pygame.image.load("assets/sprites/soldier/soldier_left_move_0.png")
+        player_left_move_1 = pygame.image.load("assets/sprites/soldier/soldier_left_move_1.png")
+        #creating arrays containing the sprites needed to render animation
+        self.player_front_move = [player_front_move_0,player_front_move_1]
+        self.player_front_move_index = 0
+        self.player_back_move = [player_back_move_0,player_back_move_1]
+        self.player_back_move_index = 0
+        self.player_right_move = [player_right_move_0,player_front_move_1]
+        self.player_right_move_index = 0
+        self.player_left_move = [player_left_move_0,player_left_move_1]
+        self.player_left_move_index = 0
+
         self.image = pygame.image.load("assets/sprites/soldier/soldier_front.png")
         self.rect = self.image.get_rect(midbottom = (200,300))
-    
+        
+
     def player_input(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
--- main.py
+++ main.py
@@ -2,27 +2,31 @@ import pygame
 from sys import exit 
 from characters import Player
 
+
 pygame.init()
 screen = pygame.display.set_mode((600,450))
 pygame.display.set_caption("Dungeon game")
 clock = pygame.time.Clock()
+
 #font = pygame.font.Font("assets/fonts/Pixeltype.ttf", 50)
+#loading map and player sprite from Player class
 map = pygame.image.load("assets/sprites/test_background.png")
 player = pygame.sprite.GroupSingle()
 player.add(Player())
 
+#game loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+    #rendering map and background on screen
     screen.fill("white")
     screen.blit(map,(0,0))
-    #player_1.movement()
-    #Player.render(player_1)
-    #screen.blit(font,(0,0))
-    #screen.blit(test, (100,100))
+    
+    #player sprite is drawn and moved/animated on the screen
     player.draw(screen)
     player.update()
+    
     pygame.display.update()
     clock.tick(60)
