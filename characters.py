ef62923c17a5d279cdad4cf34f2ef9fa1ea2f6ef
alex-pdl <apodlesny00@gmail.com>
2023-08-31 12:06:27 +0100

    Editted the player class to integrate better with pygame.

--- characters.py
+++ characters.py
@@ -1,25 +1,20 @@
 import pygame
 
-class Player:
-    x = 205
-    y = 550 
-    def __init__(self,health,bullets,image):
-        self.health = health
-        self.bullets = bullets
-        self.image_ren = pygame.image.load(image)
-        self.rect = self.image_ren.get_rect()
-
-    def movement(self):
+class Player(pygame.sprite.Sprite,x,y):
+    def __init__(self):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("assets/sprites/soldier/soldier_front.png")
+        self.rect = self.image.get_rect(midbottom = (self.x,self.y))
+    
+    def player_input(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
             self.y -= 5
-        if keys[pygame.K_a]:
-            self.x -= 5
-        if keys[pygame.K_s]:
+        elif keys[pygame.K_s]:
             self.y += 5
-        if keys[pygame.K_d]:
+        elif keys[pygame.K_a]:
+            self.x -= 5
+        elif keys[pygame.K_d]:
             self.x += 5
-    
-    def render(self):
-        screen.blit(self.image_ren, self.rect)
-
