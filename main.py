8158eb7c242af70aec330895371f6791c4eb34b8
DarioCodes1 <dario.pinatar@gmail.com>
2023-09-03 21:11:51 +0100

    added cat sprites

--- main.py
+++ main.py
@@ -6,21 +6,20 @@ from settings import level_map
 from level import Level
 from enemy import Enemy
 
-
 screen_width = 720
 screen_height = 480
 screen = pygame.display.set_mode((screen_width,screen_height))
 clock = pygame.time.Clock()
 level = Level(level_map,screen)
-
+background = pygame.image.load("assets/background/Untitled.png")
 pygame.init()
 #game loop
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT
             pygame.quit()
             sys.exit()
-    screen.fill("grey")
+    screen.blit(background, (0,0))
     level.run()
 
     pygame.display.update()
