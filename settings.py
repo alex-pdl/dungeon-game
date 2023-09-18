65e19bdc0964776575ce7985a125533f3add8f53
dariocodes1 <you@example.com>
2023-09-18 21:34:24 +0100

    added 3 levels

--- settings.py
+++ settings.py
@@ -1,13 +1,39 @@
 level_map = [
-'                                             ',
-'                                             ',
-'                        LTR                  ',
-'                                   E         ',
-'                   LTR            LTR        ',
-'                                             ',
-'                       LTR                   ',
-'   S        LTR                              ',
-'  LTTTTTR                                    ',]
+'                                                                        ',
+'                                                                        ',
+'                        LTR                                             ',
+'                                   E                                    ',
+'                   LTR            LTR     LTR             RL           T',
+'                                                          L      TL     ',
+'                       LTR                         LTR                  ',
+'   S        LTR                                                         ',
+'  LTTTTTR                                                               ',]
+
+
+
+[
+'                                                                        ',
+'                                                                        ',
+'                        LTTTR                                           ',
+'                LTR                                                     ',
+'                    T                T      TR              L      R    ',
+'                                                          T             ',
+'                      LTR                          LTR                  ',
+'   LTR      LT                                                          ',
+'                                                                        ',]
+
+[
+'                                                                        ',
+'                                                                  TTR   ',
+'                                      E                    T            ',
+'                             T    T    R            T                    ',
+'                       TR                   TR                          ',
+'                                                                        ',
+'   S             R                                                      ',
+'   T      T                                                             ',
+'                                                                        ',]
+
+
 
 tile_size = 48
 screen_width = 720
